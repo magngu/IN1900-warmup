0e8287aef7d54bac074262f5184e31bdf6fcf9b8
Magnus Gullien <magnus@Cat.local>
2022-01-03 14:48:08 +0100

    added excercise

---
+++ Mandatory excercises/population.py
@@ -0,0 +1,29 @@
+# interest_rate.py
+# Author Magnus // Jab 2022
+# ------------------------------------------------------------------------------
+import math
+
+
+def population(B, C, k, t):
+    """
+    Returns population after t hours.
+
+    B: carrying capacity
+    C: eqation constant
+    k: growth factor
+    t: time in hours, float
+
+    returns float
+    """
+    return B / (1 + C * math.e ** (-k * t))
+
+
+# Given variables
+B = 50000  # carrying capacity
+k = 0.2  # growth factor per hour^-1
+N0 = 5000  # initial condition at t=0
+C = B / N0 - 1  # Find C. t=0 --> e^(-kt) = 1
+
+# Find bacteria in colony after 24 hours
+N24 = population(B, C, k, 24)
+print(N24)
